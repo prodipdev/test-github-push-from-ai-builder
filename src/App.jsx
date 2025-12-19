a26ceda1f3b67b33374eb3932474ea74c4856818
Prodip Karati <104679918+prodipdev@users.noreply.github.com>
2025-12-19 19:44:04 +0600

    Sync: Replace all files with latest bundle

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import Stats from './components/Stats'
+import Testimonials from './components/Testimonials'
 import Footer from './components/Footer'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Features />
       <Stats />
+      <Testimonials />
       <Footer />
     </div>
   )
